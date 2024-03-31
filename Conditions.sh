f8fb3ab1c7b907e6eae92b7c93a125162ee83ede
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 20:06:28 +0530

    learning if condtions

--- Conditions.sh
+++ Conditions.sh
@@ -1,17 +1,17 @@
 x=200
-if [$x -gt 100]
+if [$x -gt 100] ; then
  echo "$x is greater than 100"
 fi
 
 y=50
-if [$y -gt 100]
+if [$y -gt 100] ; then
  echo "$y is greater than 100"
 else
    echo "$y is not less than 100"
 fi
 
    z=600
-if [$z -eq 500]
+if [$z -eq 500] ; then
  echo "$z is equal than 500"
 elif [$z -lt 500]
  echo "$z is less than 500"
