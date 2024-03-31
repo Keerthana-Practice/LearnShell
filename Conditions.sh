128f9fe1a5f8c081d4ecf09d1f3d815a13601f7b
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 20:37:19 +0530

    learning if condtions

--- Conditions.sh
+++ Conditions.sh
@@ -1,3 +1,10 @@
+echo -n “Enter the value of a: “
+read a
+if [ $a -gt 5 ]; then
+echo “$a is greater than 5”
+fi
+
+
 echo -n “Enter the value of x: “
 read x
 
@@ -5,4 +12,16 @@ if [ $x -gt 5 ]; then
 echo “$x is greater than 5”
 else
 echo “$x is not greater than 5”
+fi
+
+
+echo -n “Enter a number: “
+read num
+
+if [ $num -gt 0 ]; then
+echo “$num is a positive number.”
+elif [ $num -lt 0 ]; then
+echo “$num is a negative number.”
+else
+echo “$num is zero.”
 fi
