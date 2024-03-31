e956afec8757603e04c50d68f1fd12b6a3163895
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 20:10:31 +0530

    learning if condtions

--- Conditions.sh
+++ Conditions.sh
@@ -10,11 +10,11 @@ else
    echo "$y is not less than 100"
 fi
 
-   z=600
+z=600
 if [$z -eq 500] ; then
  echo "$z is equal than 500"
 elif [$z -lt 500]
  echo "$z is less than 500"
 else [$z -gt 500]
  echo "$z is grater than 500"
-fi #
+fi
