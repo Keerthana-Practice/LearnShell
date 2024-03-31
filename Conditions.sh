90e51d2edb56ea74c48ba8d5cf2cbadb2fdecd4c
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 20:32:39 +0530

    learning if condtions

--- Conditions.sh
+++ Conditions.sh
@@ -1,25 +1,8 @@
-x=200
-if ["$x" -gt 100]; then
- echo "$x" is greater than 100
-fi
+echo -n “Enter the value of x: “
+read x
 
-y=500
-if ["$y" -gt 100]; then
- echo "$y" is greater than 100
+if [ $x -gt 5 ]; then
+echo “x is greater than 5”
 else
-   echo "$y" is not less than 100
-fi
-
-a=$1
-if [-z "$a" ]; then
-  echo input is missing
-  exit
-fi
-
-if ["$a" -eq 500]; then
- echo "$a" is equal than 500
-elif ["$a" -lt 500]; then
- echo "$a" is less than 500
-else ["$a" -gt 500]
- echo "$a" is grater than 500
+echo “x is not greater than 5”
 fi
