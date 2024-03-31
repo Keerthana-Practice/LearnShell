ddf46291f043c12301740f422de1565461ffc6a7
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 19:58:37 +0530

    learining if condtions

---
+++ Conditions.sh
@@ -0,0 +1,20 @@
+x=200
+if [$x -gt 100]
+echo "$x is greater than 100"
+fi
+
+y=50
+if [$y -gt 100]
+ echo "$y is greater than 100"
+ else
+   echo "$y is not less than 100"
+   fi
+
+   z=600
+   if [$z -eq 500]
+   echo "$z is equal than 500"
+   elif [$z -lt 500]
+   echo "$z is less than 500"
+   else [$z -gt 500]
+   echo "$z is grater than 500"
+   fi
