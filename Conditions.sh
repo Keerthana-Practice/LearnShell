8e53ff1dfff9c990b4a18fabc0d420253bbea9d9
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 20:33:24 +0530

    learning if condtions

--- Conditions.sh
+++ Conditions.sh
@@ -2,7 +2,7 @@ echo -n “Enter the value of x: “
 read x
 
 if [ $x -gt 5 ]; then
-echo “x is greater than 5”
+echo “$x is greater than 5”
 else
-echo “x is not greater than 5”
+echo “$x is not greater than 5”
 fi
