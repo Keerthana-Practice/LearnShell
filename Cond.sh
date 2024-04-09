4d8f7b27413132a40f3187a4fdf7579b7ba64a4a
Admin <dumbalakeerthana28@gmail.com>
2024-04-09 13:37:08 +0530

    Pratice , will delete this later

--- Cond.sh
+++ Cond.sh
@@ -1,14 +1,14 @@
 echo -n "enter the value a :"
 read a
 
-if[$a -gt 10]; then
+if [$a -gt 10]; then
 echo "$a is greater than 10"
 fi
 
 enter -n "enter the value of b:"
 read b
 
-if[$b -gt 100]; then
+if [$b -gt 100]; then
 echo "$b is greater"
 else
 echo "$b is lesser than 1oo"
@@ -17,10 +17,10 @@ fi
 echo "enter the value of x:"
 read x
 
-if[$x -lt 0]; then
+if [$x -lt 0]; then
 echo "$x is a negative number"
-elif[$x -gt 0]; then
+elif [$x -gt 0]; then
 echo "$x is a positive number"
-else[$x -eq 0]; then
+else [$x -eq 0]; then
 echo "$x is equal to 0"
 fi
