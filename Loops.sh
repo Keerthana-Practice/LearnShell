7181df489df7128b611b01197200afb2467f1925
Admin <dumbalakeerthana28@gmail.com>
2024-04-09 16:13:45 +0530

    Learning loops

--- Loops.sh
+++ Loops.sh
@@ -1,10 +1,10 @@
 
 i=0
- while [ $i -lt 10]; do
+ while [$i -lt 10]; do
   echo i= $i
   i= $(($i+1))
  done
 
  for fruits in apple, banana, orange ; do
-  echo fruit = "$fruit"
+  echo fruit = "$fruits"
  done
