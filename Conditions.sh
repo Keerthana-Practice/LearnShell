fdb473270a371b879941328977a7f77bc016ed9a
Admin <dumbalakeerthana28@gmail.com>
2024-04-11 19:29:43 +0530

    MYSQL setup in YAML

--- Conditions.sh
+++ Conditions.sh
@@ -8,11 +8,11 @@ fi
 echo -n “Enter the value of x: “
 read x
 
-if [ $x -gt 5 ]; then
+`if [ $x -gt 5 ]; then
 echo “$x is greater than 5”
 else
 echo “$x is not greater than 5”
-fi
+fi`
 
 
 echo -n “Enter a number: “
